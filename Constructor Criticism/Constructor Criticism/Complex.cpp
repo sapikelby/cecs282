a0cccda91422c30d1ae9a6778270969a0bfc118c
Kelby Sapien <sapikelby@gmail.com>
2014-03-13 09:29:34 -0700

    Added .cpp and .h files

---
+++ Constructor Criticism/Constructor Criticism/Complex.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "Complex.h"
+
+#include <sstream>
+#include <string>
+Complex::Complex() : real(0), imaginary(0) {
+	std::cout << "Default constructor: " << std::endl;
+}
+Complex::Complex(double r, double i) :  real(r), imaginary(i) {
+}
+
+// accessor and mutators
+double Complex::GetReal() const {
+	return real;
+}
+
+void Complex::SetReal(double r) {
+	real = r;
+}
+
+double Complex::GetImaginary() const {
+	return imaginary;
+}
+
+void Complex::SetImaginary(double i) {
+	imaginary = i;
+}
+
+// methods
+double Complex::AbsoluteValue() const {
+	return (sqrt(real * real + imaginary * imaginary));
+}
+bool Complex::isEquals(const Complex &other) const {
+	return (real == other.real && imaginary == other.imaginary) ? true : false;
+	
+}
+std::string Complex::ToString() const {
+	std::string sign = (imaginary >= 0) ? "+" : "";
+	std::ostringstream format; 
+	format << real << sign << imaginary << "i";
+	return format.str();
+}
+Complex Complex::GetConjugate() const {
+	return Complex(real, -1 * imaginary);
+}
+
+Complex Complex::Multiply(const Complex &other) const {
+	int a = real * other.real - imaginary * other.imaginary;
+	int b = real * other.imaginary + imaginary * other.real;
+	return Complex(a, b);
+}
---
+++ Constructor Criticism/Constructor Criticism/Complex.h
@@ -0,0 +1,30 @@
+#ifndef _COMPLEX_H
+#define _COMPLEX_H
+
+#include <string>
+
+class Complex {
+private:
+	double real, imaginary;
+
+public:
+	Complex(); // default constructor
+	Complex(double r, double i); 
+
+	// accessor and mutators
+	double GetReal() const; 
+	void SetReal(double r);
+	double GetImaginary() const; 
+	void SetImaginary(double i);
+
+	// METHODS
+	double AbsoluteValue() const;
+	bool isEquals(const Complex &other) const; 
+	std::string ToString() const;
+	Complex GetConjugate() const;
+	Complex Multiply(const Complex &other) const;
+
+};
+
+
+#endif
