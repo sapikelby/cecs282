9e60c641b7da51d0aab3d63abd7d154bd9ef7454
Kelby Sapien <sapikelby@gmail.com>
2014-04-17 07:04:40 -0700

    Final working copy

--- Project2/Project2/OthelloBoard.cpp
+++ Project2/Project2/OthelloBoard.cpp
@@ -22,41 +22,37 @@ OthelloBoard::OthelloBoard() : mNextPlayer(1), mValue(0), mPassCount(0),
 };
 
 void OthelloBoard :: GetPossibleMoves(std::vector<OthelloMove *> *list) const {
-	//cout << "inside GetPossibleMoves" << endl;
 	char player = (mNextPlayer == 1) ? 1 : -1;  // switch to obtain enemy piece 
 	// checking whole board
 	for(int row = 0; row<BOARD_SIZE; row++) {
 		for(int col = 0; col<BOARD_SIZE; col++) {
-			// checking all 8 directions
+			// checking all 8 directions after finding own piece
 			if(mBoard[row][col] == player) {
 				for (int rd = -1; rd <=1; rd++) {
 					for (int cd = -1; cd <=1; cd++) {
 
 						int newR = row, newC = col, eCount = 0;
-						while (InBounds(newR, newC)) {  // &&valid) {
-							//cout << "still in loop " << "row: " << row << "col: " << col << endl;
+						while (InBounds(newR, newC)) {  
 							newR = newR + rd; 
 							newC = newC + cd;
 
-							if (mBoard[newR][newC] == 0) { // empty square, stop and add possible values
-								
+							// empty square, stop and add possible values
+							if (mBoard[newR][newC] == 0) { 
 
-								if (eCount > 0) { // set valid = false to add moves
-									//valid = false;
+								if (eCount > 0) {
 									OthelloMove* move = CreateMove();
 									*move = OthelloMove(newR,newC);
 									bool existAlready = false;
 									for(OthelloMove* i : *list) {
 										if (*i == *move) {
 											existAlready = true;
+											//cout << string(*i) << endl;
 										}
-										//break;
 									}
 									if (existAlready) {
 										delete(move);
 									}
 									else {
-										//sameRow = true;
 										list->push_back(move);
 									}
 								}
@@ -65,7 +61,8 @@ void OthelloBoard :: GetPossibleMoves(std::vector<OthelloMove *> *list) const {
 								}
 								break;
 							}
-							else if (mBoard[newR][newC] == -player) { // found opposite piece... keep searching
+							// found opposite piece... keep searching
+							else if (mBoard[newR][newC] == -player) { 
 								eCount++;
 							}
 							else { // found same piece as player's
@@ -94,15 +91,12 @@ void OthelloBoard::ApplyMove(OthelloMove *move) {
 
 	char player = (mNextPlayer == 1) ? 1 : -1;
 
-	
 	// check for pass move and update mPassCount
 	if (move->IsPass()) {
 		mPassCount++;
 		mHistory.push_back(move);
 	}
 	
-	// assume valid move, check if it's on the vector list
-	//if(move->mRow >= 0 && move->mCol >=0) {
 	else if(!move->IsPass()) {
 		mPassCount = 0; 
 		mBoard[move->mRow][move->mCol] = player;
@@ -120,7 +114,8 @@ void OthelloBoard::ApplyMove(OthelloMove *move) {
 						eCount = 0;
 						break;
 					}
-					else if (mBoard[newR][newC] == -player) { // found opposing piece
+					// found opposite piece
+					else if (mBoard[newR][newC] == -player) { 
 						eCount++;
 					}
 					else { // found same piece as player's
@@ -138,37 +133,15 @@ void OthelloBoard::ApplyMove(OthelloMove *move) {
 				}
 				
 				for (int i = 0; i < eCount; i++) {// update conquered game pieces
-					//cout << "updating pieces" << endl;
 					mBoard[newR -= rd][newC -= cd] = player;
 					mValue = mValue + player * 2;
-					//cout << "board val: " << mValue << endl;
 				}
-				
-				/*
-				while(eCount > 0) {
-					mBoard[newR -= rd][newC -= cd] = player;
-					eCount--;
-					//mValue = mValue + player * 2;
-					cout << "board val: " << mValue << endl;
-					
-				}
-				*/
 
 			}
 		}
 	}
 
-	
 	mNextPlayer = -mNextPlayer; // switch turns
-	
-	/*
-	mValue = 0; // update the value of the board
-	for(int row =0; row<BOARD_SIZE; row++) {
-		for(int col=0; col<BOARD_SIZE; col++) {
-			mValue = mValue + mBoard[row][col];
-		}
-	}
-	*/
 	
 
 };
@@ -178,8 +151,7 @@ void OthelloBoard::UndoLastMove() {
 	// mValue -- updated
 	// mNextPlayer -- updated
 
-	//OthelloMove* move;
-	if(GetMoveCount() > 0) {
+	if (GetMoveCount() > 0) {
 		OthelloMove* move = mHistory.back();
 		int flipSize = move->mFlips.size();
 
@@ -188,14 +160,14 @@ void OthelloBoard::UndoLastMove() {
 		}
 
 		else {
-			for(int i=0; i<flipSize; i++) {
+			for (int i=0; i<flipSize; i++) {
 				int newR = move->mRow, newC = move->mCol;
 				int numSwitched = move->mFlips.back().switched; 
 				int rd = move->mFlips.back().rowDelta, cd = move->mFlips.back().colDelta;
 
 				for(int ii=0; ii<numSwitched; ii++) {
-					//cout << "undoing moved pieces" << endl;
-					mBoard[newR += rd][newC += cd] *= -1;  // flip value by multiplying by -1
+					// flip value by multiplying by -1
+					mBoard[newR += rd][newC += cd] *= -1;  
 					mValue = mValue + mNextPlayer * 2;
 				}
 
@@ -204,22 +176,13 @@ void OthelloBoard::UndoLastMove() {
 
 			mBoard[move->mRow][move->mCol] = 0; // set board piece to empty
 			mValue = mValue + mNextPlayer;
-			// first delete from heap and then from vector
 		}
+
+		
+		// first delete from heap and then from vector
 		delete(mHistory.back());
 		mHistory.pop_back(); // delete move from history
 		mNextPlayer = -mNextPlayer;
 	}
 
-
-
-	/*
-	// update mValue 
-	mValue = 0; // update the value of the board
-	for(int row =0; row<BOARD_SIZE; row++) {
-		for(int col=0; col<BOARD_SIZE; col++) {
-			mValue = mValue + mBoard[row][col];
-		}
-	}
-	*/
 };
--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -1,3 +1,7 @@
+// ----------- Kelby Sapien 
+// ----------- Project 2
+// ----------- CECS 282
+
 #include "OthelloExceptions.h"
 #include "OthelloBoard.h"
 #include "OthelloView.h"
@@ -16,132 +20,13 @@ int main(int argc, char* argv[]) {
 	cout << "--- Othello Game ---" << endl << endl;
 
 	// Initialization
-	//OthelloBoard board; // the state of the game board
-	//OthelloView v(&board); // a View for outputting the board via operator<<
-	//string userInput; // a string to hold the user's command choice
-	//vector<OthelloMove *> possMoves; // a holder for possible moves
-
-	/*
-	cout << v;
-
-	board.GetPossibleMoves(&possMoves); 
-	cout << "after get poss moves" << endl;
-	int i =0;
-
-	vector<OthelloMove*>::iterator itr; 
-	for (itr = possMoves.begin(); itr != possMoves.end();itr++) {
-	cout << "------inside first for loop" << endl;
-	cout << "" << *itr << endl;
-	cout << i << endl;
-	}
-
-	for (OthelloMove* i : possMoves) { // "for each int called 'i' inside intList, do this"
-	cout << (string)*i << " ";
-	cout << "possible move" << endl;
-	}
-	cout << "after loops" << endl;
-
-	*/
-
-	/*
-	// Print the game board using the OthelloView object
-	cout << v;
-	// Print all possible moves
-	cout << "Possible moves:" << endl;
-	board.GetPossibleMoves(&possMoves);
-
-	for (int i = 0; i < possMoves.size(); i++) {
-	OthelloMove itr = *possMoves.at(i);
-	cout << (string)itr << endl;
-	//delete(&possMoves.at(i));
-
-	}
-
-	//delete(&possMoves);
-
-	//cout <<	possMoves.size() << endl;
-
-	cout << "Applied move " << possMoves.at(0) << endl;
-	board.ApplyMove(possMoves.at(0));
-
-	cout << "value: " << board.GetValue() << endl; 
-	//possMoves.clear();
-	cout << v;
-	/*
-	cout << "Possible moves:" << endl;
-	board.GetPossibleMoves(&possMoves);
-
-	for (int i = 0; i < possMoves.size(); i++) {
-	OthelloMove itr = *possMoves.at(i);
-	cout << (string)itr << endl;
-	}
-	cout << v;
-	cout << "Applied move " << possMoves.at(1) << endl;
-	board.ApplyMove(possMoves.at(4));
-	cout << v;
-
-	cout << "value: " << board.GetValue() << endl; 
-
-
-
-
-	cout << endl;
-
-
-
-
-	board.UndoLastMove();
-
-	cout << v;
-
-	cout << endl << board.GetValue();
-
-	board.ApplyMove(possMoves.at(0));
-
-	cout << v;
-
-	const vector<OthelloMove*> *history = board.GetMoveHistory();
-
-	int count = 0;
-	// use a reverse iterator
-	for (vector<OthelloMove*>::const_reverse_iterator itr = history->rbegin(); itr != history->rend();
-	itr++) { // doesn't even fit on one line :(
-	cout << "inside iterator" << endl;
-	cout << "history (" << count++ << ")" << (string)**itr << endl;
-	}
-
-
-	cout << "After history" << endl;
-
-	const vector<OthelloMove*> *history = board.GetMoveHistory();
-
-	int count = 0;
-	// use a reverse iterator
-	for (vector<OthelloMove*>::const_iterator itr = history->begin(); itr != history->end();
-	itr++) { // doesn't even fit on one line :(
-	cout << "history (" << count++ << ")" << (string)**itr << endl;
-	}
-	// memory management 
-	// delete from heap, then clear vector using clear function
-
-	//cout << board.GetMoveHistory();
-	*/
-
-
-	// Initialization
 	OthelloBoard board; // the state of the game board
 	OthelloView v(&board); // a View for outputting the board via operator<<
 	string userInput; // a string to hold the user's command choice
 	vector<OthelloMove *> possMoves; // a holder for possible moves
 	string temp;
-
-
-	//
 	vector<string> possStrings;
 
-	//sort(possMoves.begin(), possMoves.end());
-
-	//int i= 0;
 	// Main loop
 	do {
 		// Print the game board using the OthelloView object
@@ -152,10 +37,8 @@ int main(int argc, char* argv[]) {
 		// Print all possible moves
 		board.GetPossibleMoves(&possMoves);
 
-
 		for (int i = 0; i < possMoves.size(); i++) {
 			OthelloMove itr = *possMoves.at(i);
-			//cout << (string)itr << " ";
 			possStrings.push_back(string(itr));  // pushes string rep. of move onto string vector
 		}
 
@@ -178,8 +61,6 @@ int main(int argc, char* argv[]) {
 
 			OthelloException exc("Invalid move");
 			bool validMove = false;
-
-
 			// Command loop
 
 			// move (r,c)
@@ -187,9 +68,6 @@ int main(int argc, char* argv[]) {
 				istringstream moveInput(userInput);
 
 				moveInput >> temp >> temp;
-
-				//temp = userInput.substr(5,8);
-				//cout << temp << endl;
 				OthelloMove *move = board.CreateMove();
 				*move = temp;  // overloading =
 
@@ -199,45 +77,21 @@ int main(int argc, char* argv[]) {
 				istringstream getMove(temp);
 				int row = 0, col = 0;
 				getMove >> tempChar >> row >> tempChar >> col >> tempChar;
-				//cout << row << "," << col << endl;
-
-				//cout << "Move: " << (string)*move << endl;
-				// check for inbounds 
-				//move (3,2)
-				//int row = stoi(userInput.substr(6,1));
-				//int col = stoi(userInput.substr(8,1));
-				//char tempVar;
-				//cout << row << "," << col << endl;
 
 				try {
-					/*
-					if (move->IsPass()) { // only allow to pass when no moves available
-					//cout << "pass" << endl;
-					if (possMoves.empty()) {
-					cout << (string)*move << endl;
-					board.ApplyMove(move);
-					validMove = true;
-					}
-					}
-					*/
+					
 					if (board.InBounds(row, col) || move->IsPass()) {
-						//cout << "if" << endl;
-						for (OthelloMove* i : possMoves) { // "for each int called 'i' inside intList, do this"
+						for (OthelloMove* i : possMoves) { //"for each int called 'i' inside intList, do this"
 							cout << (string)*i << " ";
-							//cout << "possible move" << endl;
 							if(*i == *move) {
-								//cout << "valid move" << endl;
 								board.ApplyMove(move);
 								validMove = true;
 							}
 						}
 
-
 						if(!validMove) {
-							//cout << "throws exception" << endl;
 							throw exc;
 						}
-
 
 					}
 					else {
